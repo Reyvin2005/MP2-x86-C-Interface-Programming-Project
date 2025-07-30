70eff3dddf41e6e3277e074fa1dfd68b68b303c8
Reyvin2005 <161600359+Reyvin2005@users.noreply.github.com>
2025-07-30 20:09:26 +0800

    Updated asmsimd.asm

--- MP2-Group9/asmsimd.asm
+++ MP2-Group9/asmsimd.asm
@@ -11,29 +11,29 @@ global saxpy_simd
 ; void saxpy_simd(float A, float* X, float* Y, float* Z, int n)
 
 saxpy_simd:
-    push   rbp                      ; rsi register (non-volatile)
-    push   rbx                      ; old base pointer
+    push   rbp                      
+    push   rbx                      
     mov    rbp, rsp                 ; Set up new base pointer = current stack pointer
     add    rbp, 16                  ; Adjust rbp to account for 2 pushes (8 bytes each)
     
-    mov    rcx, [rbp + 40]          ; Load n (loop counter) from stack into rsi
+    mov    rcx, [rbp + 40]          ; Load n (loop counter) 
     movss  xmm1, xmm0               ; Copy A (scalar multiplier) from xmm0 to xmm1 for backup
     
 loop:
-    test    rcx, rcx                  ; Compare loop counter (n) with 0
-    je      end                     ; Jump to end if n == 0 (no more elements)
+    test    rcx, rcx                ; Compare loop counter
+    je      end                     ; Jump to end if n == 0 
 
-    movss   xmm2, [rdx]
-    mulss   xmm2, xmm1             ; Multiply A (in xmm0) by X[i] (pointed to by rdx)
-    addss   xmm2, [r8]              ; Add Y[i] (pointed to by r8) to the result
-    movss   [r9], xmm2              ; Store result Z[i] to memory (pointed to by r9)
+    movss   xmm2, [rdx]             ; Load X[i]
+    mulss   xmm2, xmm1              ; Multiply A by X[i] 
+    addss   xmm2, [r8]              ; Add Y[i] 
+    movss   [r9], xmm2              ; Store result Z[i] 
 
     add     rdx, 4                  ; Move X pointer to next float (4 bytes forward)
     add     r8, 4                   ; Move Y pointer to next float (4 bytes forward)
     add     r9, 4                   ; Move Z pointer to next float (4 bytes forward)
     
     dec     rcx                     ; Decrement loop counter (n--)
-    jmp     loop                    ; Jump back to start of loop
+    jmp     loop                    
 
 end:
     pop    rbx                     
